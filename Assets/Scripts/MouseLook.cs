a666b2f1e892b95c52b92712bfb8c6af9a79148a
Luskander <55799709+Luskander@users.noreply.github.com>
2020-09-18 15:59:34 +0300

    Initial commit

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public enum RotationAxes
+	{
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2 
+	}
+
+    public RotationAxes axes = new RotationAxes();
+    public float MouseSens = 3.0f;
+    public float MinAngle = -60f;
+    public float MaxAngle = 60f;
+
+    private float _rotationX = 0;
+
+    void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        if(body != null)
+		{
+            body.freezeRotation = true;
+		}
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * MouseSens, 0);
+        }
+        else if (axes == RotationAxes.MouseY)
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * MouseSens;
+            _rotationX = Mathf.Clamp(_rotationX, MinAngle, MaxAngle);
+
+            float _rotationY = transform.localEulerAngles.y;
+            transform.localEulerAngles = new Vector3(_rotationX, _rotationY, 0);
+        }
+        else
+        {
+            _rotationX -= Input.GetAxis("Mouse X") * MouseSens;
+            _rotationX = Mathf.Clamp(_rotationX, MinAngle, MaxAngle);
+
+            float delta = Input.GetAxis("Mouse Y") * MouseSens;
+            float _rotationY = transform.localEulerAngles.y + delta;
+
+            transform.localEulerAngles = new Vector3(_rotationX, _rotationY, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control script/FPS Input")]
+public class Movement : MonoBehaviour
+{
+    public float Speed = 6f;
+    public float Gravity = -9.8f;
+    private CharacterController _charController;
+
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * Speed;
+        float deltaZ = Input.GetAxis("Vertical") * Speed;
+
+        Vector3 movement = new Vector3(deltaX, 0, deltaZ);
+
+        movement = Vector3.ClampMagnitude(movement, Speed);
+        movement.y = Gravity;
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+
+        _charController.Move(movement);
+    }
+}
